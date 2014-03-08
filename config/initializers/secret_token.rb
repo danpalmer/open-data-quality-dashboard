8b2e334c1733a97300d412bc1c9b9781420afab3
Dan Palmer <dan.palmer@me.com>
2014-03-08 14:56:51 +0000

    Initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataForce::Application.config.secret_key_base = '40853600acc12bbbde3bb6ea96e4cab2930c62b57ccab59cbd11cadfee79742a4625cfa7c4ac60d5548fe2cbd088629958188c6d97b489ed4d959b69f8d125b7'
