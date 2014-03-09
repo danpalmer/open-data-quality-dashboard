66edc40c6052007e49b9e994de166dab30ff25e7
Dan Palmer <dan.palmer@me.com>
2014-03-09 09:38:18 +0000

    Pie Charts for HTTP Statuses and File Extensions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
     def stats
         @departments = Department.all
+        @extensions = Resource.select("extension").group("extension").count
+        @statuses = Resource.select("http_status").group("http_status").count
     end
 
     def leaderboard
